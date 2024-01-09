51c0bf202724f9cf37f8151b0917a6167bc58f09
YorhanSD <yorhansd08@gmail.com>
2024-01-09 00:03:53 -0300

    Program.cs Implementado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado
+Console.WriteLine("Iphone");
+
+Smartphone iphone = new Iphone("988884444", "Pro Max", "ax21", 128);
+
+Console.WriteLine(iphone.Numero);
+
+iphone.InstalarAplicativo("Facebook");
+
+iphone.Ligar();
+
+Console.WriteLine("Nokia");
+
+Smartphone nokia = new Nokia("933331111", "Super Nova", "agt8", 256);
+
+Console.WriteLine(nokia.Numero);
+
+nokia.InstalarAplicativo("YouTube");
+
+nokia.ReceberLigacao();
